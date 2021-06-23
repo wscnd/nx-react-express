ef59b54eb73a8b05943bc2dbfd1b49e85af2953b
wscnd <72019998+wscnd@users.noreply.github.com>
2021-06-23 17:30:30 -0300

    feat(store): add Header

--- apps/store/src/app/app.tsx
+++ apps/store/src/app/app.tsx
@@ -1,49 +1,55 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Typography from '@material-ui/core/Typography';
+import { Header } from '@nx-react-express/shared/components';
 
 import { getAllGames } from '../fake-api';
 import styles from './app.module.scss';
 
-export function App() {
+export default function App() {
   return (
-    <div className={styles.container}>
-      <div className={styles['games-layout']}>
-        {getAllGames().map((game) => (
-          <Card key={game.id} className={styles['game-card']}>
-            <CardActionArea>
-              <CardMedia
-                className={styles['game-card-media']}
-                title={game.name}
-                image={game.image}
-              />
-              <CardContent>
-                <Typography gutterBottom variant="h5" component="h2">
-                  {game.name}
-                </Typography>
-                <Typography variant="body2" color="textSecondary" component="p">
-                  {game.description}
-                </Typography>
+    <Fragment>
+      <Header />
+      <div className={styles.container}>
+        <div className={styles['games-layout']}>
+          {getAllGames().map((game) => (
+            <Card key={game.id} className={styles['game-card']}>
+              <CardActionArea>
+                <CardMedia
+                  className={styles['game-card-media']}
+                  title={game.name}
+                  image={game.image}
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="h2">
+                    {game.name}
+                  </Typography>
+                  <Typography
+                    variant="body2"
+                    color="textSecondary"
+                    component="p"
+                  >
+                    {game.description}
+                  </Typography>
 
-                <Typography
-                  variant="body2"
-                  color="textSecondary"
-                  component="p"
-                  className={styles['game-rating']}
-                >
-                  <strong>Rating:</strong> {game.rating}
-                </Typography>
-              </CardContent>
-            </CardActionArea>
-          </Card>
-        ))}
+                  <Typography
+                    variant="body2"
+                    color="textSecondary"
+                    component="p"
+                    className={styles['game-rating']}
+                  >
+                    <strong>Rating:</strong> {game.rating}
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+            </Card>
+          ))}
+        </div>
       </div>
-    </div>
+    </Fragment>
   );
 }
-
-export default App;
