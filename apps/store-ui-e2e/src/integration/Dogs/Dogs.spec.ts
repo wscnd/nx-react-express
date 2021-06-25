aa551aefc5d7137d290dd56fef85323592c80df9
wscnd <72019998+wscnd@users.noreply.github.com>
2021-06-25 16:10:43 -0300

    build(libs/store): generate @nrwl/react:storybook-configuration to store-ui

---
+++ apps/store-ui-e2e/src/integration/Dogs/Dogs.spec.ts
@@ -0,0 +1,7 @@
+describe('store-ui: Dogs component', () => {
+  beforeEach(() => cy.visit('/iframe.html?id=dogs--primary&knob-query='));
+    
+    it('should render the component', () => {
+      cy.get('h1').should('contain', 'Welcome to store-ui!');
+    });
+});
